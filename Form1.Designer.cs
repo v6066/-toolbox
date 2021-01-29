fccd894663f622d910bf13fb7ca71e241edcf984
v6066 <51444058+v6066@users.noreply.github.com>
2021-01-29 11:08:01 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace 工具箱
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15F);
+            this.button1.Location = new System.Drawing.Point(51, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 15F);
+            this.button2.Location = new System.Drawing.Point(215, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 95);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "PS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 15F);
+            this.button3.Location = new System.Drawing.Point(390, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 95);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "格式工厂";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("宋体", 15F);
+            this.button4.Location = new System.Drawing.Point(558, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 95);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "movavi 17";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("宋体", 15F);
+            this.button13.Location = new System.Drawing.Point(390, 386);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(114, 95);
+            this.button13.TabIndex = 0;
+            this.button13.Text = "网易云";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("宋体", 15F);
+            this.button14.Location = new System.Drawing.Point(558, 386);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(114, 95);
+            this.button14.TabIndex = 0;
+            this.button14.Text = "Lr";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("宋体", 15F);
+            this.button15.Location = new System.Drawing.Point(715, 386);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(114, 95);
+            this.button15.TabIndex = 0;
+            this.button15.Text = "OD";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button17
+            // 
+            this.button17.Font = new System.Drawing.Font("宋体", 15F);
+            this.button17.Location = new System.Drawing.Point(558, 255);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(114, 95);
+            this.button17.TabIndex = 0;
+            this.button17.Text = "微信";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Font = new System.Drawing.Font("宋体", 15F);
+            this.button18.Location = new System.Drawing.Point(715, 255);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(114, 95);
+            this.button18.TabIndex = 0;
+            this.button18.Text = "UU";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Font = new System.Drawing.Font("宋体", 15F);
+            this.button19.Location = new System.Drawing.Point(51, 386);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(114, 95);
+            this.button19.TabIndex = 0;
+            this.button19.Text = "Steam";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.Font = new System.Drawing.Font("宋体", 15F);
+            this.button20.Location = new System.Drawing.Point(215, 386);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(114, 95);
+            this.button20.TabIndex = 0;
+            this.button20.Text = "百度网盘";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.Font = new System.Drawing.Font("宋体", 15F);
+            this.button21.Location = new System.Drawing.Point(715, 121);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(114, 95);
+            this.button21.TabIndex = 0;
+            this.button21.Text = "CE";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Font = new System.Drawing.Font("宋体", 15F);
+            this.button22.Location = new System.Drawing.Point(51, 255);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(114, 95);
+            this.button22.TabIndex = 0;
+            this.button22.Text = "腾讯课堂";
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // button23
+            // 
+            this.button23.Font = new System.Drawing.Font("宋体", 15F);
+            this.button23.Location = new System.Drawing.Point(215, 255);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(114, 95);
+            this.button23.TabIndex = 0;
+            this.button23.Text = "QQ";
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
+            // 
+            // button24
+            // 
+            this.button24.Font = new System.Drawing.Font("宋体", 15F);
+            this.button24.Location = new System.Drawing.Point(390, 255);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(114, 95);
+            this.button24.TabIndex = 0;
+            this.button24.Text = "VS2019";
+            this.button24.UseVisualStyleBackColor = true;
+            this.button24.Click += new System.EventHandler(this.button24_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(873, 633);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "工具箱";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 工具箱
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\百度网盘\BaiduNetdisk\BaiduNetdisk.exe");
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"E:\BaiduNetdiskDownload\Cheat Engine 7.0\Cheat Engine.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\PS\Adobe Photoshop CC 2018\Photoshop.exe");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\DW\Adobe Dreamweaver 2020\Dreamweaver.exe");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\FormatFactory\FormatFactory.exe");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\UU\Netease\UU");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"steam://rungameid/795630");
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\QQ\Bin\QQScLauncher.exe");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\微信\WeChat\WeChat.exe");
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\vs\Common7\IDE\devenv.exe");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"E:\steam\Steam.exe");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {//???
+            Process.Start(@"D:\网易云音乐\CloudMusic\cloudmusic.exe");
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\腾讯课堂\TXEDU\2.0.0.17\bin\TXEDU.exe");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\LR\Adobe Lightroom Classic CC\Lightroom.exe");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+           //后续添加
+        }
+    }
+}
